434fb34ffba0ba29329d2db4d54dd93a63c077ec
B1up0ison <salmanhashim@hotmail.ca>
2021-10-22 22:00:47 -0400

    cleaned server

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 import socket
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -8,12 +7,9 @@ port = 1337
 s.bind((host,port))
 s.listen(0)
 
-con,addr=s.accept()
-print("connected with", addr)
+con,client=s.accept()
+print("connected with", client)
 
 while True:
-    #messag=input("send message to client: ")
-    #con.send(messag.encode())
-    #print("waiting for response")
     c_message=con.recv(1024)
     print("",c_message.decode())
